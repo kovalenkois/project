6fd315812c869857b006fcf745f23776c238b8d6
Igor <kovalenkois23@gmail.com>
2022-10-24 19:47:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//const result = confirm ('Are you here?');
+//console.log (result);
+
+//const answer = prompt ('How old are you?', '');
+// console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt ('Как тебя зовут?','');
+//answers[1] = prompt ('Фамилия?','');
+//answers[2] = prompt ('Сколько тебе лет?','');
+
+//console.log(answers);
+//console.log('arr' + ' - object');
+//console.log(5 + +'5');
+
+//let incr = 10,
+    //decr = 10;
+
+//incr++;
+//decr--;
+
+//console.log(incr++);
+//console.log(decr--);
+
+//const isChecked = true;
+//const isClose = false; 
+
+//console.log(isChecked || isClose);
+
+console.log(5+2 != 5);
